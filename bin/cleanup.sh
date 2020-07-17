14372fcc41d7a322fbb173610d5f9d1842ee4d90
Andrea Moneti <moneti@iap.fr>
2020-07-17 18:18:57 +0200

    made more flexible

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -13,18 +13,30 @@
 # - but delete other products in the Data/rnnn/ch?/bcd dirs
 # - delete contents of temp dir
 #-----------------------------------------------------------------------------
+if [[ "${@: -1}" =~ 'dry' ]]; then dry=T; else dry=F; fi
 
-#cd $WRK
-echo $PWD
+# check that WRK is defined
+if [[ -z "$WRK" ]]; then 
+    echo " ERROR: \$WRK (workdir) environment variable not defined ... quitting" 
+    exit 20
+fi
+
+cd $WRK; echo " --> $PWD"
+field=$(pwd | cut -d\/ -f3)   # nominally name of Spitzer field 
 
 if [ -e supermopex.py ]; then
 	odir=$(grep 'OutputDIR  =' supermopex.py | cut -d\' -f2 | tr -d \/)
-	rdir=$(grep 'RawDataDir =' supermopex.py | cut -d\' -f2 | tr -d \/)
+	RAW=$(grep 'RawDataDir =' supermopex.py | cut -d\' -f2 | tr -d \/)
 else
-	echo "supermopex.py not found; "
-	odir=Products
-	rdir=Data
+	echo "supermopex.py not found; quiting"  
+	exit 0
 fi
+
+#-----------------------------------------------------------------------------
+
+naor=$(ls -d $RAW/r???* | wc -l)
+echo ">> Field is $field; raw data linked from $RAW ... "
+echo ">> Found $naor AORs in $RAW ... OK?"  #; exit
 
 # move or delete?
 if [ $# -eq 1 ]; then
@@ -32,20 +44,28 @@ if [ $# -eq 1 ]; then
 		echo " move products to $1"
 		keep="T"
 	else
-		echo " remove products ..."
-		keep="F"
+		echo "ERROR: output directory $1 not found - quitting"
+		exit 3
 	fi
+else
+	echo " remove products ..."
+	keep="F"
 fi
-#exit
+
+exit
+echo ">> ...  ^C to quit if not correct"  
+sleep 10 #; exit
+
+#-----------------------------------------------------------------------------
 
 # check that raw data dir exists
-if [ ! -d $rdir ]; then
-	echo "ERROR: $rdir not found - quitting!"
+if [ ! -d $RAW ]; then
+	echo "ERROR: $RAW not found - quitting!"
 	exit 1
 fi
 
 # delete intermediate products from raw data dir
-for f in $rdir/r*/ch?/bcd; do 
+for f in $RAW/r*/ch?/bcd; do 
 	echo "- now clean $f"
 	rm -f $f/SPI*_[f-z]*.fits $f/SPI*.tbl
 done
@@ -57,10 +77,10 @@ if [[ $keep == "T" ]]; then
 else
 	echo "## delete command etc. files"
 	rm -f *.sh *.py *.out build.* *.err addkey* FIF.tbl cdf/log_*  # supermopex.py
-	rm -rf medians $odir countFiles.dat irac.log make_tiles*
+	rm -rf medians $odir countFiles.dat irac.log 
 fi
 
-rm -rf temp __pycache__ *.qall 
+rm -rf temp/* __pycache__ 
 
 
 exit 0
